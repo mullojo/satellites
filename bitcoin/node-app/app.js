541ad42debf4243fec8854db40e44a12465089da
Bob <rpmullaly@gmail.com>
2024-08-27 14:52:13 -0700

    Added bitcoind node

---
+++ bitcoin/node-app/app.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const rpcURL = 'http://bitcoind:18332';  // Docker service name
+const rpcUser = process.env.RPC_USER;
+const rpcPassword = process.env.RPC_PASSWORD;
+
+async function getBlockchainInfo() {
+    try {
+        const response = await axios.post(rpcURL, {
+            jsonrpc: "1.0",
+            id: "curltest",
+            method: "getblockchaininfo",
+            params: []
+        }, {
+            auth: {
+                username: rpcUser,
+                password: rpcPassword
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log('Blockchain Info:', response.data);
+    } catch (error) {
+        console.error('Error fetching blockchain info:', error.message);
+        if (error.response) {
+            console.error('Error response:', error.response.data);
+        }
+    }
+}
+
+getBlockchainInfo();
