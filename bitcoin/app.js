1307f170e030373b36c0c2f52c66e9ef6b1da56d
Bob <rpmullaly@gmail.com>
2024-08-28 01:01:50 -0700

    added shared network and configs

--- bitcoin/app.js
+++ bitcoin/app.js
@@ -2,7 +2,7 @@ import Client from 'bitcoin-core';
 
 // Initialize the Bitcoin client
 const client = new Client({
-    network: process.env.NETWORK,
+    // network: process.env.NETWORK,
     username: process.env.RPC_USER,
     password: process.env.RPC_PASSWORD,
     host: process.env.HOST,  // Docker service name or appropriate host
